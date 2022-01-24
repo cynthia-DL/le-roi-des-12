3b4c8b6fb877ca3235e90f6ad791efd0102dd44c
cynthia-DL <cynthia.d.l@outlook.fr>
2022-01-24 20:52:27 +0100

    Adding basic files and mockup functions

---
+++ src/De.java
@@ -0,0 +1,13 @@
+public class De {
+    private int face;
+    private int value;
+
+    public De(){
+        this.face = newJet();
+        this.value = this.face;
+    }
+
+    public int newJet(){
+        return 0;
+    }
+}
---
+++ src/Manche.java
@@ -0,0 +1,5 @@
+public class Manche {
+
+    public void endRound(){}
+    public void newTurn(){}
+}
