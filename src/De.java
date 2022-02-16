979d4cc3b31c53f3031d6a9c69293f7f73d162df
Alex <maizeroialex@gmail.com>
2022-02-16 09:24:36 +0100

    Complétion de classe De

--- src/De.java
+++ src/De.java
@@ -8,6 +8,26 @@ public class De {
     }
 
     public int newJet(){
-        return 0;
+        // Calcul d'un nombre aléatoire entre 1 et 12 compris
+        this.value= 1+(int)(Math.random()*12);
+        this.face=this.value;
+
+        return value;
+    }
+
+    public int getFace() {
+        return face;
+    }
+
+    public void setFace(int face) {
+        this.face = face;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
     }
 }
